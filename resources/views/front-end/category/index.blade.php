613536fa55c179953ea3c697256b8ac77f7d72ad
changha8888 <changha8888@gmmail.com>
2019-04-02 11:39:24 -0700

    category

--- resources/views/front-end/category/index.blade.php
+++ resources/views/front-end/category/index.blade.php
@@ -8,18 +8,17 @@
             <a href="{{url('/post-detail/'.$post->slug)}}"><img src="{{url('/uploads/news/'.$post->images)}}" alt=""></a>
         </div>
         <div class="post-data">
-            <a href="#" class="post-catagory">Finance</a>
+            <a href="#" class="post-catagory">{{$post->category->name}}</a>
             <a href="#" class="post-title">
                 <h6>{{$post->name}}</h6>
             </a>
             <div class="post-meta">
-                <p class="post-author">By <a href="#">Christinne Williams</a></p>
                 <p class="post-excerp">{{$post->intro}}</p>
                 <!-- Post Like & Post Comment -->
-                <div class="d-flex align-items-center">
+                <!-- <div class="d-flex align-items-center">
                     <a href="#" class="post-like"><img src="img/core-img/like.png" alt=""> <span>392</span></a>
                     <a href="#" class="post-comment"><img src="img/core-img/chat.png" alt=""> <span>10</span></a>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
